2e0afb79e7bba206d18fae11cb5acc3468adc9c8
Khalid <alamutukhalid@gmail.com>
2026-03-05 17:28:36 +0100

    feat: added new project - oasis

--- src/componenets/works/Data.js
+++ src/componenets/works/Data.js
@@ -1,17 +1,17 @@
 export const Data = [
 	{
-		id: 1,
+		id: 2,
 		image:
 			'https://cdn.jsdelivr.net/gh/KhalidHayKay/Assets@a932fb07c6e37ecaf9dadd1136c2b307e4332855/Portfolio/work_messaging_app.jpg',
 		title: 'PeriChat',
 		description:
-			'PeriChat Frontend is a real-time messaging platform built with <strong>React</strong>, <strong>Vite</strong>, and <strong>Tailwind</strong>. Users can start one-on-one conversations, create and join group chats, and enjoy a smooth messaging experience across devices.',
+			'PeriChat Frontend is a real-time messaging platform built with <strong>React (vite)</strong>, and <strong>Tailwind</strong>. Users can start one-on-one conversations, create and join group chats, and enjoy a smooth messaging experience across devices.',
 		// description: 'PeriChat Frontend is a dynamic chat interface built with <strong>React</strong>, <strong>Vite</strong>, and <strong>Tailwind</strong>. It handles real-time messaging, group chats, and conversation management through seamless API integration, designed for speed and reliability.',
 		github: 'https://github.com/KhalidHayKay/Perichat',
 		website: 'https://perichat-livid.vercel.app',
 	},
 	{
-		id: 3,
+		id: 4,
 		image:
 			'https://cdn.jsdelivr.net/gh/KhalidHayKay/Assets@master/Portfolio/work_weather.jpg',
 		title: 'Weather4Cast',
@@ -21,7 +21,7 @@ export const Data = [
 		website: 'https://myweather4cast.netlify.app',
 	},
 	{
-		id: 5,
+		id: 6,
 		image:
 			'https://cdn.jsdelivr.net/gh/KhalidHayKay/Assets@master/Portfolio/work_task_schedular.jpg',
 		title: 'Taskify',
@@ -31,7 +31,7 @@ export const Data = [
 		website: null,
 	},
 	{
-		id: 2,
+		id: 3,
 		image:
 			'https://cdn.jsdelivr.net/gh/KhalidHayKay/Assets@master/Portfolio/work_rest_api.jpg',
 		title: 'Capsule Auth API',
@@ -42,7 +42,7 @@ export const Data = [
 			'https://capsule-ptt2.onrender.com/docs/api?token=e3b7f84f-b35a-41c7-90c2-8f7a6b912c49',
 	},
 	{
-		id: 4,
+		id: 5,
 		image:
 			'https://cdn.jsdelivr.net/gh/KhalidHayKay/Assets@eeb09308b0ad4a3fe5abcc770981e002cfa858a5/Portfolio/work_messaging_api.png',
 		title: 'PeriChat API',
@@ -50,5 +50,15 @@ export const Data = [
 			'PeriChat API is a real-time messaging backend built with <strong>Laravel</strong>. It features a well-structured database design and supports event-driven communication via WebSockets — forming the backbone of PeriChat’s live chat system.',
 		github: 'https://github.com/KhalidHayKay/PeriChat-BE',
 		website: null,
+	},
+	{
+		id: 1,
+		image:
+			'https://cdn.jsdelivr.net/gh/KhalidHayKay/Assets@163d4cffc484ac1730015cc59dc369a898e56d68/Portfolio/work_oasis.png',
+		title: 'Oasis',
+		description:
+			'A scalable full-stack e-commerce platform developed using <strong>Next.js</strong>, <strong>Laravel</strong>, and <strong>PostgreSQL</strong>. The application provides a seamless shopping experience with product browsing, cart management, order processing, and secure checkout, supported by a powerful backend for managing products, users, and transactions.',
+		github: 'https://github.com/khalidhaykay/Oasis',
+		website: 'https://oasis-seven-puce.vercel.app',
 	},
 ];
